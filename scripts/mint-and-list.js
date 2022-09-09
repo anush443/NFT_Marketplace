3429fc155bd78d54641dd950eab20a9b53ff712c
anushgs <anush443@gmail.com>
2022-09-09 18:22:23 +0530

    scripts

---
+++ scripts/mint-and-list.js
@@ -0,0 +1,26 @@
+const { etehrs, ethers } = require("hardhat")
+
+const PRICE = ethers.utils.parseEther("0.1")
+
+const mintAndList = async () => {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const basicNft = await ethers.getContract("BasicNft")
+    console.log("minting")
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+    console.log("approving...")
+    const approvalTx = await basicNft.approve(nftMarketplace.address, tokenId)
+    approvalTx.wait(1)
+    console.log("listing")
+    const tx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
+    await tx.wait(1)
+    console.log("NFT Listed!")
+}
+
+mintAndList()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
