d653ad9bb3ca91a32b02edd103c110540c0282bf
anushgs <anush443@gmail.com>
2022-09-15 21:57:56 +0530

    Move blocks added

---
+++ utils/move-blocks.js
@@ -0,0 +1,20 @@
+const { network } = require("hardhat")
+async function sleep(timeInMs) {
+    return new Promise((resolve) => setTimeout(resolve, timeInMs))
+}
+
+async function moveBlocks(amount, sleepAmount = 0) {
+    console.log("moving blocks....")
+    for (let index = 0; index < amount; index++) {
+        await network.provider.request({
+            method: "evm_mine",
+            params: [],
+        })
+
+        if (sleepAmount) {
+            console.log(`sleeping for ${sleepAmount}`)
+            await sleep(sleepAmount)
+        }
+    }
+    console.log(`Moved ${amount} blocks`)
+}
